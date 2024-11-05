c42be0afe8afa6968b74f10b54f5a8af33504395
brayan <brayangafaro475@gmail.com>
2024-11-05 19:37:32 -0400

    creado menu y zona de preguntas v1

---
+++ client/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [isMenuActive, setIsMenuActive] = useState(true);
+
+  const menuOff = () => {
+    setIsMenuActive(false);
+  };
+
+  return (
+    <main className="screen">
+      {/* <section className={isMenuActive ? "menu" : "inactive"}> */}
+      <section className={"inactive"}>
+        <h1>TriviaGame</h1>
+        <button className="boton_general" onClick={menuOff}>
+          Jugar!
+        </button>
+      </section>
+
+      {/* <section className={isMenuActive ? "inactive" : "question"}> */}
+      <section className={"zona_preguntas"}>
+        <p className="pregunta">
+          quetion: placeholder para preguntas que no va a ser definitivo
+        </p>
+        <div className="zona_respuestas">
+          <button className="boton_general">placeholder</button>
+          <button className="boton_general">placeholder</button>
+          <button className="boton_general">placeholder</button>
+          <button className="boton_general">placeholder</button>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
