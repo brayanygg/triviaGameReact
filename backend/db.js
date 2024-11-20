dafdd0f891ae1bfee84773effc1fae22d7dc0cfb
brayan <brayangafaro475@gmail.com>
2024-11-19 23:27:51 -0400

    agg favicon

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 import { MongoClient } from "mongodb";
+import { key } from "./pass.js";
 
 const client = new MongoClient(
-  "mongodb+srv://kratos:NgqHxvYeVBXBQkhv@cluster0.0zngv.mongodb.net/"
+  `mongodb+srv://kratos:${key}@cluster0.0zngv.mongodb.net/`
 );
 
 export const dbConnection = async () => {
