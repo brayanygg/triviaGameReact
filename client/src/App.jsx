7d4703e376b980caf4bde136315d02befd45a765
brayan <brayangafaro475@gmail.com>
2024-11-24 20:18:06 -0400

    juego terminado en alfa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,74 +1,90 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [isMenuActive, setIsMenuActive] = useState(true);
+  const [seccion, setSeccion] = useState("menu");
   const [preguntas, setPreguntas] = useState(null);
-  const [botonDesactivado, setBotonDesactivado] = useState(true);
+  const [cargando, setCargando] = useState(true);
   const [count, setCount] = useState(0);
   const [vidas, setVidas] = useState(3);
   const [puntaje, setPuntaje] = useState(0);
 
-  const traerPreguntas = async () => {
-    try {
-      const respuesta = await axios.get("http://localhost:5000/preguntas");
-      setPreguntas(respuesta.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const aleatorizarPreguntas = useCallback((antePreguntas) => {
+    antePreguntas.sort(() => Math.random() - 0.5);
 
-  const aleatorizarPreguntas = () => {
-    const toRandom = [...preguntas];
-    toRandom.sort(() => Math.random() - 0.5);
-    setPreguntas(toRandom);
-
-    preguntas.forEach((pregunta) => {
+    antePreguntas.forEach((pregunta) => {
       pregunta.respuestas.sort(() => Math.random() - 0.5);
-      console.log(pregunta.respuestas);
     });
 
-    console.log("fin");
-  };
+    setPreguntas(antePreguntas);
+  }, []);
+
+  const traerPreguntas = useCallback(async () => {
+    try {
+      const respuesta = await axios.get("http://localhost:5000/preguntas");
+      aleatorizarPreguntas(respuesta.data);
+    } catch (error) {
+      console.error("Error al traer las preguntas", error);
+    } finally {
+      setCargando(false);
+    }
+  }, [aleatorizarPreguntas]);
 
   useEffect(() => {
     traerPreguntas();
-    setTimeout(() => {
-      setBotonDesactivado(false);
-    }, 2000);
-  }, []);
+  }, [traerPreguntas]);
 
   const menuOff = () => {
-    aleatorizarPreguntas();
-    setIsMenuActive(false);
+    setSeccion("zona_preguntas");
   };
 
-  const revision = (respuesta, correcta) => {
-    if (vidas < 2) {
-      alert("has perdido");
-    }
+  const finalOff = () => {
+    setVidas(3);
+    setCount(0);
+    setPuntaje(0);
+    aleatorizarPreguntas(preguntas);
+    setSeccion("menu");
+  };
 
-    if (respuesta == correcta) {
-      console.log("correcta");
-    } else {
-      console.log("incorrecto");
-      setVidas(vidas - 1);
-    }
+  const revision = useCallback(
+    (respuesta, correcta) => {
+      setPuntaje((prevPuntaje) => {
+        if (respuesta === correcta) {
+          return prevPuntaje + 100;
+        } else {
+          setVidas((prevVidas) => {
+            if (prevVidas === 1) {
+              setSeccion("zona_final");
+              return 0;
+            } else {
+              return prevVidas - 1;
+            }
+          });
+          return prevPuntaje;
+        }
+      });
+      setCount((prevCount) => {
+        if (prevCount === preguntas.length - 1) {
+          setSeccion("zona_final");
+          return 0;
+        } else {
+          return prevCount + 1;
+        }
+      });
+    },
+    [preguntas]
+  );
 
-    if (preguntas.length - 1 > count) {
-      console.log("youre inside");
-
-      setCount(count + 1);
-    }
-    console.log(count);
+  const reiniciarMaxPuntaje = () => {
+    setPuntaje(0);
   };
 
   return (
     <main className="screen">
-      <section className={isMenuActive ? "menu" : "inactive"}>
+      <section className={seccion == "menu" ? "menu" : "inactive"}>
         <h1>TriviaGame</h1>
-        {botonDesactivado ? (
+        {cargando ? (
           <div>Cargando...</div>
         ) : (
           <button className="boton_general" onClick={menuOff}>
@@ -77,8 +93,9 @@ function App() {
         )}
       </section>
 
-      <section className={isMenuActive ? "inactive" : "zona_preguntas"}>
-        {console.log(count)}
+      <section
+        className={seccion == "zona_preguntas" ? "zona_preguntas" : "inactive"}
+      >
         {preguntas ? (
           <div key={preguntas[count]._id}>
             <img src={preguntas[count].imgRoute} alt={preguntas[count]._id} />
@@ -98,14 +115,27 @@ function App() {
         ) : (
           ""
         )}
+        <div className="zona_numeros">
+          <div className="zona_vidas">
+            <span>❤️</span>
+            <span>x{vidas}</span>
+          </div>
+          <div className="zona_puntuacion">
+            <span>Puntuación: {puntaje}</span>
+          </div>
+        </div>
       </section>
 
-      <section className="inactive">
+      <section className={seccion == "zona_final" ? "zona_final" : "inactive"}>
         <p className="terminado">Juego terminado</p>
-        <p className="puntuaciones">Puntaje: 100</p>
-        <p className="max puntuaciones">Maximo puntaje: 600</p>
-        <button className="boton_restaurar">X</button>
-        <button className="volver boton_general">Volver a inicio</button>
+        <p className="puntuaciones">Puntaje: {puntaje}</p>
+        <p className="max puntuaciones">Maximo puntaje: {puntaje}</p>
+        <button onClick={reiniciarMaxPuntaje} className="boton_restaurar">
+          X
+        </button>
+        <button onClick={finalOff} className="volver boton_general">
+          Volver a inicio
+        </button>
       </section>
     </main>
   );
